416094b8608dbd18532842a73d8ce17e293c59c9
autumn <autumn@localhost.localdomain>
2024-02-27 06:46:17 -0800

    update

--- new/simulation_model/FD.v
+++ new/simulation_model/FD.v
@@ -1,12 +1,11 @@
 module FD #(
-    parameter INIT = 1'b0  // 默认初始值为0
+    parameter INIT = 1'b0  
 )(
     input wire C,          // Clock input
     input wire D,          // Data input
     output reg Q           // Data output
 );
 
-// 在时钟上升沿捕获数据，并设置初始值
 initial begin
     Q = INIT;
 end
